f0a3079469a1e674597d1936ac9236d08863abc8
Matiasahumada <pagosptcmatias@gmail.com>
2025-01-05 12:36:17 -0300

    cree el maquetado dle login, con la captura de los datos

---
+++ front/src/app/register/page.tsx
@@ -0,0 +1,8 @@
+const registerPage: React.FC = () => {
+  return (
+    <div>
+     <h1>register</h1>
+    </div>
+  );
+}
+export default registerPage;
---
+++ front/src/app/todo/page.tsx
@@ -0,0 +1,8 @@
+const todo: React.FC = () => {    
+    return (
+        <div>
+            <h1>Todo</h1>
+        </div>
+    )
+}
+export default todo;
